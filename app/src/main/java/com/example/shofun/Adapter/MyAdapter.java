bb7603f99e57d8355c8adde77f1f2a45c1603d0f
Harvian Khusnan Hafidz <harvian7x@gmail.com>
2022-11-18 14:29:39 +0700

    develeop_v4

--- app/src/main/java/com/example/shofun/Adapter/MyAdapter.java
+++ app/src/main/java/com/example/shofun/Adapter/MyAdapter.java
@@ -11,12 +11,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.shofun.Models.CartModel;
 import com.example.shofun.Models.ShirtCart;
 import com.example.shofun.R;
+import com.example.shofun.eventbus.UpdateCartEvent;
+import com.example.shofun.listener.ICartLoadListener;
+import com.example.shofun.listener.IRecyclerViewClickListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import org.greenrobot.eventbus.EventBus;
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,10 +39,12 @@ import butterknife.Unbinder;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private Context context;
     private List<ShirtCart> shirtCartList;
+    private ICartLoadListener iCartLoadListener;
 
-    public MyAdapter(Context context, List<ShirtCart> shirtCartList) {
+    public MyAdapter(Context context, List<ShirtCart> shirtCartList, ICartLoadListener iCartLoadListener) {
         this.context = context;
         this.shirtCartList = shirtCartList;
+        this.iCartLoadListener = iCartLoadListener;
     }
 
     @NonNull
@@ -43,10 +59,78 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         Glide.with(context)
                 .load(shirtCartList.get(position).getImage())
                 .into(holder.imageView);
-        holder.txtPrice.setText(new StringBuilder("RP").append(shirtCartList.get(position).getPrice()));
+        holder.txtPrice.setText(new StringBuilder("IDR").append(shirtCartList.get(position).getPrice()));
         holder.txtName.setText(new StringBuilder("").append(shirtCartList.get(position).getName()));
 
+        holder.setListener((view, adapterPosition) -> {
+            addToCart(shirtCartList.get(position));
+        });
 
+
+    }
+
+    private void addToCart(ShirtCart shirtcart) {
+        DatabaseReference userCart = FirebaseDatabase
+                .getInstance()
+                .getReference("Cart")
+                .child("UNIQUE_USER_ID");
+        userCart.child(shirtcart.getKey())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()){
+                            CartModel cartModels = snapshot.getValue(CartModel.class);
+                            cartModels.setQuantity(cartModels.getQuantity()+1);
+                            Map<String,Object> updateData = new HashMap<>();
+                            updateData.put("quantity",cartModels.getQuantity());
+                            updateData.put("total price",cartModels.getQuantity()*Float.parseFloat(cartModels.getPrice()));
+
+                            userCart.child(shirtcart.getKey())
+                                    .updateChildren(updateData)
+                                    .addOnSuccessListener(unused -> {
+                                        iCartLoadListener.onCartLoadFailed("Add To Cart Succes");
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            iCartLoadListener.onCartLoadFailed(e.getMessage());
+                                        }
+                                    });
+
+                        }
+                        else {
+                            CartModel cartModel = new CartModel();
+                            cartModel.setName(shirtcart.getName());
+                            cartModel.setImage(shirtcart.getImage());
+                            cartModel.setKey(shirtcart.getKey());
+                            cartModel.setPrice(shirtcart.getPrice());
+                            cartModel.setQuantity(1);
+                            cartModel.setTotalPrice(Float.parseFloat(shirtcart.getPrice()));
+
+                            userCart.child(shirtcart.getKey())
+                                    .setValue(cartModel)
+                                    .addOnSuccessListener(unused -> {
+                                iCartLoadListener.onCartLoadFailed("Add To Cart Succes");
+                            })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            iCartLoadListener.onCartLoadFailed(e.getMessage());
+                                        }
+                                    });
+
+
+                        }
+                        EventBus.getDefault().postSticky(new UpdateCartEvent());
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        iCartLoadListener.onCartLoadFailed(error.getMessage());
+                    }
+                });
     }
 
     @Override
@@ -55,19 +139,34 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.imageview)
         ImageView imageView;
         @BindView(R.id.txtName)
         TextView txtName;
         @BindView(R.id.txtPrice)
         TextView txtPrice;
+        @BindView(R.id.crtbttn)
+        ImageView cartbutton;
+
+
+        IRecyclerViewClickListener listener;
+
+        public void setListener(IRecyclerViewClickListener listener) {
+            this.listener = listener;
+        }
 
         private Unbinder unbinder;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             unbinder = ButterKnife.bind(this, itemView);
+            cartbutton.setOnClickListener(this);
+        }
 
+
+        @Override
+        public void onClick(View view) {
+            listener.onRecylerClick(view, getAdapterPosition());
         }
     }
 }
--- app/src/main/java/com/example/shofun/Katalog.java
+++ app/src/main/java/com/example/shofun/Katalog.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -14,6 +15,7 @@ import android.widget.RelativeLayout;
 import com.example.shofun.Adapter.MyAdapter;
 import com.example.shofun.Models.CartModel;
 import com.example.shofun.Models.ShirtCart;
+import com.example.shofun.eventbus.UpdateCartEvent;
 import com.example.shofun.listener.ICartLoadListener;
 import com.example.shofun.listener.IDshirtLoadListener;
 import com.example.shofun.utils.SpaceItemDecoration;
@@ -23,6 +25,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.nex3z.notificationbadge.NotificationBadge;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,12 +51,33 @@ public class Katalog extends AppCompatActivity implements IDshirtLoadListener, I
     ICartLoadListener cartLoadListener;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        if(EventBus.getDefault().hasSubscriberForEvent(UpdateCartEvent.class))
+            EventBus.getDefault().removeStickyEvent(UpdateCartEvent.class);
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN,sticky = true)
+
+    public void onUpdateCart(UpdateCartEvent event){
+        councartItem();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_katalog);
 
         init();
         LoadShirtFromFirebase();
+        councartItem();
 
     }
 
@@ -88,11 +115,13 @@ shirtLoadListener.onShirtLoadFailed(error.getMessage());
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.addItemDecoration(new SpaceItemDecoration());
+
+        btncart.setOnClickListener(v -> startActivity(new Intent(this, CartActivity.class)));
     }
 
     @Override
     public void omShirtLoadSucces(List<ShirtCart> shirtCartList) {
-        MyAdapter adapter = new MyAdapter(this,shirtCartList);
+        MyAdapter adapter = new MyAdapter(this,shirtCartList,cartLoadListener);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
     }
@@ -106,10 +135,44 @@ shirtLoadListener.onShirtLoadFailed(error.getMessage());
     @Override
     public void onCartLoadSucces(List<CartModel> cartModelList) {
 
+        int cartSum = 0;
+        for (CartModel cartModel: cartModelList)
+            cartSum += cartModel.getQuantity();
+        badge.setNumber(cartSum);
+
     }
 
     @Override
     public void onCartLoadFailed(String Message) {
+        Snackbar.make(mainlayout,Message,Snackbar.LENGTH_LONG).show();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        councartItem();
+    }
+
+    private void councartItem() {
+        List<CartModel> cartmodels = new ArrayList<>();
+        FirebaseDatabase
+                .getInstance().getReference("Cart")
+                .child("UNIQUE_USER_ID")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot cartsnaphot:snapshot.getChildren()){
+                            CartModel cartmodel = cartsnaphot.getValue(CartModel.class);
+                            cartmodel.setKey(cartsnaphot.getKey());
+                            cartmodels.add(cartmodel);
+                        }
+                        cartLoadListener.onCartLoadSucces(cartmodels);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        cartLoadListener.onCartLoadFailed(error.getMessage());
+                    }
+                });
     }
 }
